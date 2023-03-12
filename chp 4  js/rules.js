f35b170b20a112c12568271ce92ab5fe4d79888b
Bismakhalique <121749134+Bismakhalique@users.noreply.github.com>
2023-03-12 01:19:27 -0800

    Add files via upload

---
+++ chp 4  js/rules.js
@@ -0,0 +1,34 @@
+// //        <----QUESTION#1---->
+
+// var w="atrooba", x="samia" ,y="bisma" ,z="hani";
+
+
+// //        <----QUESTION#2---->
+
+// // Legal variable..
+// var firstName="bisma";
+// var _age="19";
+// var $price="infinty";
+// var fullName="bisma khalique";
+// var numStudent=10;
+
+// // Legal variable..
+// var 123="10";
+// var my name="bisma";
+// var var="wrong";
+// var my-name="bisma khalique";
+// var true="1";
+
+
+
+//        <----QUESTION#3---->
+const heading = document.getElementById("heading");
+heading.textContent = "Rules for naming JS variables";
+
+const paragraph = document.getElementById("paragraph");
+paragraph.innerHTML = `Variable names can only contain <strong>letters</strong>, <strong>numbers</strong>, <strong>underscores</strong>, and <strong>dollar signs</strong>.<br>
+For example <code>$my_1stVariable</code><br>
+Variables must begin with a <strong>letter</strong>, <strong>underscore</strong>, or <strong>dollar sign</strong>.<br>
+For example <code>$name</code>, <code>_name</code>, or <code>name</code><br>
+Variable names are case <strong>sensitive</strong>.<br>
+Variable names should not be JS <strong>keywords</strong>.`;
